a6ac1ffa809a4018124c948f25b4246c9dedf98c
luiz bernardi <luizbernardi37@gmail.com>
2024-11-11 13:12:53 -0300

    fix: refatorando

--- estoque/src/main/java/com/estoque/estoque/model/User.java
+++ estoque/src/main/java/com/estoque/estoque/model/User.java
@@ -1,12 +1,10 @@
 package com.estoque.estoque.model;
 
-import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
